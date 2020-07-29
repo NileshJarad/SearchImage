d3c697832aff4540a4962ba1089af1d1118ffe0d
Nilesh Jarad <nlshjarad7@gmai.com>
2020-07-29 11:49:40 +0530

    Added date formatting for comments

--- app/src/main/java/com/search_image/utils/BindingAdapter.kt
+++ app/src/main/java/com/search_image/utils/BindingAdapter.kt
@@ -1,11 +1,15 @@
 package com.search_image.utils
 
+import android.annotation.SuppressLint
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import coil.api.load
 import com.google.android.material.snackbar.Snackbar
 import com.search_image.R
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 @BindingAdapter("visibility")
@@ -16,14 +20,21 @@ fun View.setVisibility(value: Boolean) {
 @BindingAdapter("showSnack")
 fun View.shoeSnack(message: String?) {
     message?.let {
-            Snackbar.make(this, it, Snackbar.LENGTH_LONG).show()
+        Snackbar.make(this, it, Snackbar.LENGTH_LONG).show()
     }
 }
 
 @BindingAdapter("imageUrl")
-fun ImageView.loadImage(imageUrl : String){
+fun ImageView.loadImage(imageUrl: String) {
     this.load(imageUrl) {
         crossfade(true)
         placeholder(R.drawable.ic_image_24px)
     }
+}
+
+
+@SuppressLint("SimpleDateFormat")
+@BindingAdapter("commentDate")
+fun TextView.loadImage(commentDate: Long) {
+    text = SimpleDateFormat("dd, MMM   hh:mm a ").format(Date(commentDate))
 }
